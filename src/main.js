cea427602dc08a67a576a61a1b41f958a0e58840
David Mendenhall <davidmendenhallact4333@gmail.com>
2025-01-24 20:18:53 -0700

    Added code to set up base sceen

---
+++ src/main.js
@@ -0,0 +1,59 @@
+// @ts-check
+
+const main2dCanvas = document.createElement("canvas");
+document.body.appendChild(main2dCanvas);
+
+/** @type {CanvasRenderingContext2D} */
+// @ts-ignore
+const ctx = main2dCanvas.getContext("2d");
+
+const resizeCanvas = () => {
+    main2dCanvas.width = main2dCanvas.getBoundingClientRect().width;
+    main2dCanvas.height = main2dCanvas.getBoundingClientRect().height;
+    ctx.resetTransform();
+    const width = main2dCanvas.width;
+    const height = main2dCanvas.height;
+    ctx.translate(width / 2, height / 2);
+    // Scale to normalize the coordinate space to -1 to 1
+    ctx.scale(height / 2, -height / 2);
+    ctx.fillRect(0, 0, 0.5, 0.5);
+};
+
+const clear = () => {
+    ctx.save();
+    ctx.resetTransform();
+    ctx.clearRect(0, 0, main2dCanvas.width, main2dCanvas.height);
+    ctx.restore();
+    // TODO code to clear other canvases
+};
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+let oldTime = -1;
+/**
+ *
+ * @param {number} timeMs
+ * @returns
+ */
+const frame = (timeMs) => {
+    if (oldTime < 0) {
+        oldTime = timeMs;
+        return;
+    }
+    const deltaTimeMs = timeMs - oldTime;
+    oldTime = timeMs;
+
+    draw(deltaTimeMs);
+    requestAnimationFrame(frame);
+};
+
+/**
+ *
+ * @param {number} deltaMs Time since last frame in milliseconds
+ */
+const draw = (deltaMs) => {
+    clear();
+};
+
+requestAnimationFrame(frame);
